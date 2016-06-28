8102ed33b1cf549d2305d88bcf5bf88dc71ddbbf
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-28 23:20:13 +0100

    Tests for class methods passing

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,7 +1,18 @@
 class Quote < ActiveRecord::Base
 
-  def self.filter(character)
-
+  def self.filter(query)
+    character = find_character(query)
+    Quote.where(character: character).shuffle[0]
   end
 
+  private
+
+  def self.find_character(query)
+    characters = Quote.uniq.pluck(:character)
+    characters.each { |name| return name if matches?(name, query) }
+  end
+
+  def self.matches?(name, query)
+    return name.downcase.include?(query.downcase)
+  end
 end
