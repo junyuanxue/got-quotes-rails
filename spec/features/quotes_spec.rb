66ee0f0111e6c0596027310186f1e79bb71da59a
Junyuan Xue <junyuanxue.is@gmail.com>
2016-06-30 22:03:43 +0100

    stubbed out api call in feature test

--- spec/features/quotes_spec.rb
+++ spec/features/quotes_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 describe 'quotes' do
+  let!(:data) {'{"quote": "I drink and I know things.", "character": "Tyrion"}'}
+
+  before do
+    stub_request(:get, /got-quotes.herokuapp.com/).
+      to_return(body: data, status: 200, headers: {})
+  end
+
   it 'adds a new quote when fetch quote button is clicked' do
     visit '/quotes/new'
     click_button 'Fetch Quote'
